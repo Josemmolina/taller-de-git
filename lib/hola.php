106343dc8a88ccf4a3881f56c045bf989498f578
jose <salaorex@gmail.com>
2023-10-31 09:02:12 +0100

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,6 @@
 <?php
-//Autor: Jose <jmolrod845@g.educaand.es>
-//El nombre por defecto es mundo
-$nombre = isset($argv[1]) ? $argv[1]:"Mundo";
-@print "Hola {$argv[1]}\n";
+// Autor: Jose Maria Molina <jmolrod845@g.educaand.es>
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
+@print "Hola, {$nombre}\n"
 ?>
